97e4cdead4f503a463c48455d752d3f39e13a17f
Juanjo Bazán <jjbazan@gmail.com>
2024-03-18 18:20:11 +0100

    Add preprocessing

---
+++ preprocess_paper.rb
@@ -0,0 +1,117 @@
+require "theoj"
+require "yaml"
+
+issue_id = ENV["ISSUE_ID"]
+repo_url = ENV["REPO_URL"]
+repo_branch = ENV["PAPER_BRANCH"]
+acceptance = ENV["COMPILE_MODE"] == "accepted"
+
+journal = Theoj::Journal.new(Theoj::JOURNALS_DATA[:jcon])
+issue = Theoj::ReviewIssue.new(journal.data[:reviews_repository], issue_id)
+issue.paper = Theoj::Paper.from_repo(repo_url, repo_branch)
+submission = Theoj::Submission.new(journal, issue, issue.paper)
+
+paper_path = issue.paper.paper_path
+
+if paper_path.nil?
+  system("echo 'CUSTOM_ERROR=Paper file not found.' >> $GITHUB_ENV")
+  raise "   !! ERROR: Paper file not found"
+else
+  system("echo 'paper_file_path=#{paper_path}' >> $GITHUB_OUTPUT")
+end
+
+begin
+  metadata = submission.article_metadata
+rescue Theoj::Error => e
+  system("echo 'CUSTOM_ERROR=#{e.message}.' >> $GITHUB_ENV")
+  raise "   !! ERROR: Invalid submission metadata"
+end
+
+if acceptance && metadata[:published_at].to_s.strip.empty?
+  metadata[:published_at] = Time.now.strftime("%Y-%m-%d")
+end
+
+metadata[:submitted_at] = "0000-00-00" if metadata[:submitted_at].to_s.strip.empty?
+metadata[:published_at] = "0000-00-00" if metadata[:published_at].to_s.strip.empty?
+
+metadata[:editor].transform_keys!(&:to_s)
+metadata[:authors].each {|author| author.transform_keys!(&:to_s) }
+metadata.transform_keys!(&:to_s)
+
+paper_dir = File.dirname(paper_path)
+
+metadata_file_path = paper_dir + "/paper-metadata.yaml"
+File.open(metadata_file_path, "w") do |f|
+  f.write metadata.to_yaml
+end
+
+for k in ["title", "authors", "affiliations", "keywords", "bibliography"]
+  if issue.paper.paper_metadata[k].to_s == ""
+    missing_metadata_key_msg = "Invalid submission metadata, #{k} not present in metadata"
+    system("echo 'CUSTOM_ERROR=#{missing_metadata_key_msg}.' >> $GITHUB_ENV")
+    raise "   !! ERROR: #{missing_metadata_key_msg}"
+  end
+end
+
+# ENV variables or default for issue/volume/year
+journal_issue = ENV["JLCON_ISSUE"] || metadata["issue"]
+volume = ENV["JLCON_VOLUME"] || metadata["volume"]
+year = ENV["JLCON_YEAR"] || metadata["year"]
+journal_name = Theoj::JOURNALS_DATA[:jcon][:name]
+
+authors = issue.paper.paper_metadata["authors"]
+affiliations = issue.paper.paper_metadata["affiliations"]
+
+header_tex = <<~HEADERTEX
+  % **************GENERATED FILE, DO NOT EDIT**************
+
+  \\title{#{metadata["title"]}}
+
+  #{ authors.map {|author| '\\author[' + author["affiliation"].to_s + ']{' + author["name"] + '}'}.join("\n") }
+  #{ affiliations.map {|affiliation| '\\affil[' + affiliation["index"].to_s + ']{' + affiliation["name"] + '}' }.join("\n") }
+
+  \\keywords{#{metadata["tags"].join(", ")}}
+
+  \\hypersetup{
+  pdftitle = {#{metadata["title"]}},
+  pdfsubject = {JuliaCon #{year} Proceedings},
+  pdfauthor = {#{authors.map { |author| author["name"] }.join(", ")}},
+  pdfkeywords = {#{metadata["tags"].join(", ")}},
+  }
+
+HEADERTEX
+
+header_file_path = paper_dir + "/header.tex"
+File.open(header_file_path, 'w') do |f|
+  f.write header_tex
+end
+
+journal_dat_tex = <<~JOURNALDATTEX
+  % **************GENERATED FILE, DO NOT EDIT**************
+
+  \\def\\@journalName{#{journal_name}}
+  \\def\\@volume{#{volume}}
+  \\def\\@issue{#{journal_issue}}
+  \\def\\@year{#{year}}
+JOURNALDATTEX
+
+journal_dat_file_path = paper_dir + "/journal_dat.tex"
+File.open(journal_dat_file_path, 'w') do |f|
+  f.write journal_dat_tex
+end
+
+bib_tex = <<~BIBTEX
+  % **************GENERATED FILE, DO NOT EDIT**************
+
+  \\bibliographystyle{juliacon}
+  \\bibliography{#{issue.paper.bibliography_path}}
+
+BIBTEX
+
+bib_file_path = paper_dir + "/bib.tex"
+File.open(bib_file_path, 'w') do |f|
+  f.write bib_tex
+end
+
+
+system("echo 'paper_dir=#{paper_dir}' >> $GITHUB_OUTPUT")
